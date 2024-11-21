e2eef034dac2c3604e4ed773e6c01bbee463bf5f
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-21 18:58:03 +0530

    Setup placeholder Side Navigation

---
+++ src/lib/components/SideNav.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import SideNavButton from "./SideNavButton.svelte";
+    import { ArrowRightEndOnRectangle, Cog8Tooth, Icon } from "svelte-hero-icons";
+    let extended = $state(false);
+</script>
+
+
+<div class="flex flex-col justify-start fixed h-full bg-black transition-all duration-700 {extended ? 'bg-green-500 w-16':'bg-blue-500 w-56'}">
+    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
+    <button class="absolute bottom-16 bg-orange-500 h-16 transition-all duration-700 flex items-center justify-center {extended ? 'w-16': 'w-56'}" onclick={()=>extended = !extended}>
+        <Icon src={ArrowRightEndOnRectangle} class="size-8 stroke-black"></Icon>
+    </button>
+</div>
---
+++ src/lib/components/SideNavButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Icon } from "svelte-hero-icons";
+    let el = $props();
+</script>
+
+<button class="bg-orange-500 h-16 transition-all duration-700 flex items-center justify-center {el.open ? 'w-16': 'w-56'}">
+    <Icon src={el.icon} class="size-8 stroke-black"></Icon>
+</button>
