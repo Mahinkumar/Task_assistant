b18a9894a15a343443a55a625eec5653799afd4d
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-21 19:08:55 +0530

    Update Animations and textbox

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -6,11 +6,12 @@
 
 
 <div class="flex flex-col justify-start fixed h-full bg-black transition-all duration-700 {extended ? 'bg-green-500 w-16':'bg-blue-500 w-56'}">
-    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth}></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
     <button class="absolute bottom-16 bg-orange-500 h-16 transition-all duration-700 flex items-center justify-center {extended ? 'w-16': 'w-56'}" onclick={()=>extended = !extended}>
         <Icon src={ArrowRightEndOnRectangle} class="size-8 stroke-black"></Icon>
     </button>
--- src/lib/components/SideNavButton.svelte
+++ src/lib/components/SideNavButton.svelte
@@ -3,6 +3,7 @@
     let el = $props();
 </script>
 
-<button class="bg-orange-500 h-16 transition-all duration-700 flex items-center justify-center {el.open ? 'w-16': 'w-56'}">
-    <Icon src={el.icon} class="size-8 stroke-black"></Icon>
+<button class="bg-orange-500 h-16 transition-all duration-700 flex items-center justify-evenly {el.open ? 'w-16': 'w-56'}">
+    <Icon src={el.icon} class="size-8 stroke-black {el.class}"></Icon>
+    <div class="p-2 bg-black {!el.open ? 'w-36':'w-0 hidden'}"></div>
 </button>
