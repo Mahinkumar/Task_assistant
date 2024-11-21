b73712d54ef3c6fa1f7b7d28a6295299af9884d5
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-21 19:24:41 +0530

    Add Options for redirect

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
     import SideNavButton from "./SideNavButton.svelte";
     import { ArrowRightEndOnRectangle, Cog8Tooth, Icon } from "svelte-hero-icons";
-    let extended = $state(false);
+    let closed = $state(true);
 </script>
 
 
-<div class="flex flex-col justify-start fixed h-full bg-black transition-all duration-700 {extended ? 'bg-green-500 w-16':'bg-blue-500 w-56'}">
-    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
-    <SideNavButton open={extended} icon={Cog8Tooth} class="bg-black"></SideNavButton>
-    <button class="absolute bottom-16 bg-orange-500 h-16 transition-all duration-700 flex items-center justify-center {extended ? 'w-16': 'w-56'}" onclick={()=>extended = !extended}>
+<div class="flex flex-col justify-start fixed h-full bg-black transition-all duration-700 {closed ? 'bg-green-500 w-16':'bg-blue-500 w-56'}">
+    <SideNavButton open={closed} icon={Cog8Tooth} class="bg-black" url="/"></SideNavButton>
+    <SideNavButton open={closed} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={closed} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={closed} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={closed} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <SideNavButton open={closed} icon={Cog8Tooth} class="bg-black"></SideNavButton>
+    <button class="absolute bottom-16 bg-orange-500 h-16 transition-all duration-700 flex items-center justify-center {closed ? 'w-16': 'w-56'}" onclick={()=>closed = !closed}>
         <Icon src={ArrowRightEndOnRectangle} class="size-8 stroke-black"></Icon>
     </button>
 </div>
+<div class="h-screen transition-all duration-1000  {!closed ? 'w-56' : 'w-16'}"></div>
--- src/lib/components/SideNavButton.svelte
+++ src/lib/components/SideNavButton.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+    import { goto } from '$app/navigation'
 	import { Icon } from "svelte-hero-icons";
+    
     let el = $props();
+    
 </script>
 
-<button class="bg-orange-500 h-16 transition-all duration-700 flex items-center justify-evenly {el.open ? 'w-16': 'w-56'}">
+<button onclick={()=>goto(el.url)} class="bg-orange-500 h-16 transition-all duration-700 flex items-center justify-evenly {el.open ? 'w-16': 'w-56'}">
     <Icon src={el.icon} class="size-8 stroke-black {el.class}"></Icon>
     <div class="p-2 bg-black {!el.open ? 'w-36':'w-0 hidden'}"></div>
 </button>
