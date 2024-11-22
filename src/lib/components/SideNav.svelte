6196be7c29c3498acba9b131aaa1bbe35b6e7b9c
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-22 18:53:03 +0530

    Update Hide Menu Button

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -10,8 +10,9 @@
     <SideNavButton open={closed} icon={ClipboardDocumentCheck} class="" url="/tasks" text="Dashboard"></SideNavButton>
     <SideNavButton open={closed} icon={PencilSquare} class="" url="/notes" text="Dashboard"></SideNavButton>
     <SideNavButton open={closed} icon={RectangleStack} class="" url="/memories" text="Dashboard"></SideNavButton>
-    <button class="absolute bottom-16 h-16 transition-all duration-700 flex items-center justify-center {closed ? 'w-16': 'w-56'}" onclick={()=>closed = !closed}>
-        <Icon src={ArrowRightEndOnRectangle} class="size-8 stroke-black"></Icon>
+    <button onclick={()=>closed = !closed} class="absolute bottom-16 hover:bg-neutral-200 h-16 transition-all duration-700 flex items-center justify-evenly {closed ? 'w-16 pr-2': 'w-56'}">
+        <div class="transition-all text-lg {!closed ? 'w-36 opacity-100 duration-700 pl-4 text-start':'w-0 opacity-0 duration-500'} ">Hide&nbsp;Menu</div>
+        <Icon src={ArrowRightEndOnRectangle} class="size-8 stroke-black {!closed ? 'rotate-180' : 'rotate-0'}"></Icon>
     </button>
 </div>
 <div class="h-screen transition-all duration-700  {!closed ? 'w-56' : 'w-16'}"></div>
