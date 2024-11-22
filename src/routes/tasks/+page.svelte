c5d881b8efc651d1c4523e7692d4620ed21ddce3
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-22 21:01:00 +0530

    Add Task system

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,1 +1,26 @@
-<div class="text-2xl p-5">Tasks Page</div>
+<script lang="ts">
+    import { writable } from "svelte/store";
+    import { onMount } from "svelte";
+
+    let data = {'hello':'bye'};
+
+    let count = writable([data]);
+
+    onMount(async () => {
+    const storedCount = sessionStorage.getItem("count");
+    
+    if (storedCount) {
+        await count.update((value) => JSON.parse(storedCount));
+    }
+    
+    count.subscribe((value) => {
+        sessionStorage.setItem("count", JSON.stringify(value));
+    });
+    });
+ </script>
+
+  <div class="p-5 text-xl">
+    <button on:click={() => count.update((n) => {n.push({'hello':'bye'}); return n})}>Increment</button>
+    <val>{$count}</val>
+    Home Page
+  </div>
