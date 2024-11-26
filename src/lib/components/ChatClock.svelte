999332930203e22f2df9417d6b94c8e5b0e352c7
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-26 09:19:12 +0530

    Update Animations

--- src/lib/components/ChatClock.svelte
+++ src/lib/components/ChatClock.svelte
@@ -4,8 +4,8 @@
 </script>
 <div class="h-96 w-full flex flex-col items-center">
     <div class="w-48 h-8 border-2 border-gray-500 flex rounded-b-lg justify-center items-center">
-        <button class="h-8 w-10 hover:text-gray-800  flex items-center justify-center {clockmode == 'Work' ? 'text-black' : 'text-gray-500'}" onclick={()=>clockmode='Work'}><BookOpenText/></button>
-        <button class="h-8 w-10  hover:text-gray-800 flex items-center justify-center {clockmode == 'Rest' ? 'text-black' : 'text-gray-500'}" onclick={()=>clockmode='Rest'}><BookText/></button>
-        <button class="h-8 w-20 text-lg font-semibold">{clockmode}</button>
+        <button class="transition-all duratiion-700 h-8 w-10 hover:text-gray-800  flex items-center justify-center {clockmode == 'Work' ? 'text-black' : 'text-gray-400'}" onclick={()=>clockmode='Work'}><BookOpenText/></button>
+        <button class="transition-all duratiion-700 h-8 w-10 hover:text-gray-800 flex items-center justify-center {clockmode == 'Rest' ? 'text-black' : 'text-gray-400'}" onclick={()=>clockmode='Rest'}><BookText/></button>
+        <button class="transition-all duratiion-700 h-8 w-20 text-lg font-semibold" onclick={()=> clockmode = (clockmode == 'Rest' ? 'Work' : 'Rest')}>{clockmode}</button>
     </div>
 </div>
