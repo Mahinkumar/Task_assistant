db5ea342328db94b27b27bea04104276cecfe6c7
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-26 16:11:44 +0530

    Update Toast Layout

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -17,22 +17,24 @@
    
   <script lang="ts">
     import { createToaster, melt } from '@melt-ui/svelte'
+    import { fade } from 'svelte/transition'
   </script>
    
   <div use:portal class="absolute right-4 bottom-4 space-y-4">
     {#each $toasts as { id, data } (id)}
-      <div use:melt={$content(id)} class="bg-green-500 rounded-2xl w-64 h-32 p-5">
-        <div>
-          <div>
-            <h3 use:melt={$title(id)}>
-              {data.title}
+      <div use:melt={$content(id)} transition:fade class="bg-gray-200 rounded-2xl w-64 pt-2 pb-2 flex flex-col items-center justify-start ring-2 ring-zinc-600">
+        <div class="flex flex-col items-center justify-center p-1">
+            <h3 use:melt={$title(id)} class="font-bold text-start">
+              From : {data.title}
               <span style:color={data.color}></span>
             </h3>
-            <div use:melt={$description(id)}>
+            <div use:melt={$description(id)} class="text-start pl-4 pr-4">
               {data.description}
             </div>
+          <div class="flex justify-evenly items-center w-full">
+          <button use:melt={$close(id)} class="underline text-red-500" aria-label="close notification">Close</button>
+          <button use:melt={$close(id)} class="underline text-green-700" aria-label="Accept notification">Ok</button>
           </div>
-          <button use:melt={$close(id)} aria-label="close notification"> X </button>
         </div>
       </div>
     {/each}
--- src/lib/components/TopNav.svelte
+++ src/lib/components/TopNav.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-    import { Cog6Tooth, Icon, CalendarDays, Map, User, Bell, BellSlash } from "svelte-hero-icons";
+	import { Cog6Tooth, Icon, CalendarDays, Map, User, Bell, BellSlash } from 'svelte-hero-icons';
 	let mute = $state(false);
 
-	import { addToast } from '$lib/components/Toast.svelte'
+	import { addToast } from '$lib/components/Toast.svelte';
 
- 
 	function create() {
 		addToast({
-		data: {
-			title: 'Toggle State',
-			description: 'State Toggled',
-			color: 'green'
-		}
-		})
+			data: {
+				title: 'Notification Helper',
+				description: mute
+					? 'Muted Notifications Indefinitely. You will not recieve any alerts.'
+					: 'Unmuted All Notifications. You will Recieve alerts for all notifications now.',
+				color: 'green'
+			}
+		});
 	}
 </script>
 
@@ -42,7 +43,10 @@
 			><Icon src={CalendarDays} class="size-7 stroke-black" /></button
 		>
 		<button
-            onclick={()=>{mute = !mute; create()}}
+			onclick={() => {
+				mute = !mute;
+				create();
+			}}
 			class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 lg:flex hidden justify-center items-center"
 			>{#if !mute}
 				<Icon src={Bell} class="size-7 stroke-black" />
