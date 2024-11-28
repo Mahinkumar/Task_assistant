d4e0e80ef8d77094dd93116daf9c8312a9aa9438
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-28 13:17:13 +0530

    Setup Internal markdown parser

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -1,1 +1,5 @@
-<div class="text-2xl p-5">Notes Page</div>
+
+<div class="text-2xl p-5 flex flex-col">
+    Notes Page
+    <a href="/notes/new" class="text-lg underline text-blue-700">Go to notepad</a>
+</div>
