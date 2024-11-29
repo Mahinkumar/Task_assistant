d7da2795e1c7776fcdec0a2ef77af0ee2a6fcd1b
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-29 21:39:54 +0530

    Improve Accessiblity for Card components

--- src/lib/components/dashboard/notes_mini.svelte
+++ src/lib/components/dashboard/notes_mini.svelte
@@ -6,7 +6,7 @@
   
   <div class="bg-violet-300 p-4 rounded-lg shadow-md m-4 w-64 h-64">
     <div class="font-bold text-xl mb-2">{name}</div>
-    <p class="text-sm text-gray-700 mb-4">{details}</p>
-    <div class="text-xs text-gray-500">{date}</div>
+    <p class="text-sm mb-4">{details}</p>
+    <div class="text-xs">{date}</div>
   </div>
   
