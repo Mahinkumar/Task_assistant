928fa655f305e60ce7a0ba886acc62bc91858e35
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-30 14:11:57 +0530

    Switch to auto adapter

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import type { ActionData } from './$types';
 
 	let { form }: { form: ActionData } = $props();
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+//import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
