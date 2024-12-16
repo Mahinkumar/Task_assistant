e57396bedcf321eb0e9e5ee1a4aa491159e9d299
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-16 20:21:06 +0530

    Add New utility to notes

--- src/routes/[[app]]/notes/edit/[slug]/+page.svelte
+++ src/routes/[[app]]/notes/edit/[slug]/+page.svelte
@@ -8,19 +8,135 @@
 	let val = $state('');
 	let Button = $state(0);
 	$effect(() => (val = DOMPurify.sanitize(marked(markdown))));
+
+	import {
+		Bell,
+		Bold,
+		BookDownIcon,
+		Calendar,
+		Code,
+		Heading1,
+		Heading2,
+		Heading3,
+		Italic,
+		LucideFullscreen,
+		NotebookPenIcon,
+		SaveIcon,
+		SquareCheckBigIcon,
+		Strikethrough,
+		Table
+	} from 'lucide-svelte';
+	import { getElementById } from '@melt-ui/svelte/internal/helpers';
+
+	const checkbox = () => {markdown += '- [ ] '};
+	const bold = () => {markdown += '**Bold Text** '}
+
+	const h1 = () => { markdown += '# ' };
+	const h2 = () => { markdown += '## ' };
+	const h3 = () => { markdown += '### ' };
+
+	const italics = () => { markdown += '*Italic Text*' };
+
+	const strikethrough = () => { markdown += '~~Strikethrough Text~~' };
+
+	const code = () => { markdown += '`Code`' };
+
+	const table = () => {
+	markdown += '| Column 1 | Column 2 |\n';
+	markdown += '|-----------|-----------|\n';
+	};
 </script>
 
-<div class="flex flex-col w-full h-full pl-2 pr-2 ml-3 mr-3">
-	<Notestoolbar/>
+<form class="flex flex-col w-full h-full pl-2 pr-2 ml-3 mr-3 bg-white" id="form">
+	<div
+		class="flex justify-evenly min-w-max items-center gap-4 rounded-lg m-2 ml-0 mr-3 py-3 text-neutral-700 shadow-sm"
+	>
+		<div class="flex w-1/3">
+			<input
+				class="link nowrap flex-shrink-0 form-textarea w-full border-gray-400 pl-5"
+				type="text"
+				placeholder="Note_Name"
+			/>
+		</div>
+		<div class="flex w-2/3 justify-evenly items-center space-x-4">
+			<button class="ml-auto rounded-md bg-white py-1 font-medium text-lg"><SaveIcon /></button>
+			<button class="ml-auto rounded-md bg-white py-1 font-medium text-lg"><BookDownIcon /></button>
+			<div class="separator"></div>
+			<div class="flex items-center gap-1">
+				<button class="item" aria-label="bold" onclick={bold}>
+					<Bold />
+				</button>
+				<button class="item" aria-label="italic" onclick={italics}>
+					<Italic />
+				</button>
+				<button class="item" aria-label="strikethrough" onclick={strikethrough}>
+					<Strikethrough />
+				</button>
+			</div>
+			<div class="separator"></div>
+			<div class="flex items-center gap-1">
+				<button class="item" aria-label="Add H1 Title" onclick={h1}>
+					<Heading1 />
+				</button>
+				<button class="item" aria-label="Add H2 Title" onclick={h2}>
+					<Heading2 />
+				</button>
+				<button class="item" aria-label="Add H3 Title" onclick={h3}>
+					<Heading3 />
+				</button>
+			</div>
+			<div class="separator"></div>
+			<div class="flex items-center gap-1">
+				<button class="item" aria-label="Add Table" onclick={table}>
+					<Table />
+				</button>
+				<button class="item" aria-label="Add Code" onclick={code}>
+					<Code />
+				</button>
+				<button class="item" aria-label="Add CheckBox" onclick={checkbox}>
+					<SquareCheckBigIcon />
+				</button>
+			</div>
+			<div class="separator"></div>
+			<a href="/#" class="link nowrap flex-shrink-0"> Edited 2 hours ago </a>
+			<input type="date" class="form-input ml-auto rounded-md bg-white px-3 font-medium text-lg w-42"/>
+			<div class="separator"></div>
+			<div class="flex items-center gap-2">
+				<button class="item" aria-label="align center">
+					<NotebookPenIcon />
+				</button>
+				<button class="item" aria-label="align-right" onclick={()=>{getElementById('form')?.requestFullscreen()}}>
+					<LucideFullscreen />
+				</button>
+			</div>
+		</div>
+	</div>
+
 	<div class="flex justify-stretch h-screen">
 		<textarea
 			class="p-5 w-[50%] h-full border-2 border-gray-400 focus:border-violet-100"
 			bind:value={markdown}
 			placeholder="Enter your notes here. Markdown is supported !"
 		></textarea>
-		<div class="w-[50%] p-5 h-full border-2 border-l-0 border-gray-400 ">
+		<div class="w-[50%] p-5 h-full border-2 border-l-0 border-gray-400">
 			<div class="prose max-w-fit">{@html val}</div>
 		</div>
 	</div>
-</div>
+</form>
 
+<style lang="postcss">
+	.item {
+		padding: theme('spacing.1');
+		border-radius: theme('borderRadius.md');
+
+		&:focus {
+			@apply ring-2;
+		}
+	}
+
+	.separator {
+		width: 1px;
+		background-color: theme('colors.neutral.300');
+		align-self: stretch;
+	}
+</style>
