42739ae2a00c5fba2addd868137c0be6204a8865
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-17 21:52:34 +0530

    Improve Notes Elements

--- src/lib/components/note/note.svelte
+++ src/lib/components/note/note.svelte
@@ -1,24 +1,62 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-	import { Pencil, Trash2 } from "lucide-svelte";
+	import { goto } from '$app/navigation';
+	import { ChevronRight, Ellipsis, GalleryThumbnails, Pencil, Trash2 } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
 
-    export let name: String | null = "";
-    export let details: String | null = "";
-    export let date: String | null | Date = "";
-    export let sno: number | undefined;
+	let data = $props();
 
-    if (!sno){sno = 0};
-
-
+	let show_settings = $state(false);
+	let is_selected = $state(false);
+	if (!data.sno) {
+		data.sno = 0;
+	}
 </script>
-  
 
-<form class="w-72 h-72 bg-slate-200 p-5 relative" method="post">
-    <div class="font-bold text-xl mb-2">{name}</div>
-    <p class="text-sm mb-4">{details}</p>
-    <div class="text-xs">{date}</div>
-    <div class="flex space-x-2 absolute right-4 bottom-8">
-    <button class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center" formaction="?/delete"><Trash2/></button>
-    <button class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center" type="button" onclick={()=>{goto("/notes/edit/"+sno.toString())}}><Pencil/></button>
-    </div>
+<form
+	class="w-72 h-72 bg-slate-200 p-5 relative {is_selected ? 'ring-2 ring-red-700' : ''}"
+	method="post"
+	ondblclick={() => {
+		is_selected = !is_selected;
+	}}
+>
+	<div class="font-bold text-xl mb-2">{data.name}</div>
+	<p class="text-sm mb-4">{data.details}</p>
+	<div class="text-xs">{data.date}</div>
+	<div class="flex space-x-2 absolute right-4 bottom-8 transtion-all duration-700">
+		<button
+			class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center"
+			type="button"
+			onclick={() => {
+				show_settings = !show_settings;
+			}}
+			>{#if show_settings}
+				<ChevronRight />
+			{:else}
+				<Ellipsis />
+			{/if}</button
+		>
+		{#if show_settings}
+			<button
+				in:fade
+				class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center"
+				type="button"
+				onclick={() => {
+					goto('/notes/view/' + data.sno.toString());
+				}}><GalleryThumbnails /></button
+			>
+			<button
+				in:fade
+				class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center"
+				formaction="?/delete"><Trash2 /></button
+			>
+			<button
+				in:fade
+				class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center"
+				type="button"
+				onclick={() => {
+					goto('/notes/edit/' + data.sno.toString());
+				}}><Pencil /></button
+			>
+		{/if}
+	</div>
 </form>
