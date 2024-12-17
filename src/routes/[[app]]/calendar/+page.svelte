2639e8199bd59d0941b223345133e6b16f8ab5d2
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-17 17:56:23 +0530

    Add New calendar ui

--- src/routes/[[app]]/calendar/+page.svelte
+++ src/routes/[[app]]/calendar/+page.svelte
@@ -1,1 +1,18 @@
-<div class="text-2xl p-5">Calendar Page</div>
+<script>
+    // @ts-ignore
+    import Calendar from '@event-calendar/core';
+    // @ts-ignore
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek',
+        events: [
+            // your list of events
+        ]
+    };
+</script>
+
+<div class="w-screen p-5">
+    <Calendar {plugins} {options} />
+</div>
--- src/routes/[[app]]/notes/+page.svelte
+++ src/routes/[[app]]/notes/+page.svelte
@@ -1,4 +1,15 @@
+<script lang="ts">
+	import NotesMini from '$lib/components/dashboard/notes_mini.svelte';
+	import datemap from '$lib/date';
+	import { get_type_data, shared } from '../shared.svelte';
+	let notes = $derived(get_type_data(shared.all_data, 'notes'));
+</script>
+
 <div class="text-2xl p-5 flex flex-col">
 	Notes Page
-	<a href="/notes/new" class="text-lg underline text-blue-700">Create a New Note</a>
+	<a href="/notes/new" class="text-lg underline text-blue-700 grid grid-cols-3">Create a New Note</a>
+	{#each notes as note}
+		<NotesMini name={note.Title} details={note.Content} date={datemap(note.SetDate).fulldate} />
+	{/each}
+	
 </div>
