04e11da9fc386dc0dc8fc647e2e796970266f497
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-17 21:36:27 +0530

    Add Notes View Page

---
+++ src/lib/components/note/note.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { Pencil, Trash2 } from "lucide-svelte";
+
+    export let name: String | null = "";
+    export let details: String | null = "";
+    export let date: String | null | Date = "";
+    export let sno: number | undefined;
+
+    if (!sno){sno = 0};
+
+
+</script>
+  
+
+<form class="w-72 h-72 bg-slate-200 p-5 relative" method="post">
+    <div class="font-bold text-xl mb-2">{name}</div>
+    <p class="text-sm mb-4">{details}</p>
+    <div class="text-xs">{date}</div>
+    <div class="flex space-x-2 absolute right-4 bottom-8">
+    <button class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center" formaction="?/delete"><Trash2/></button>
+    <button class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center" type="button" onclick={()=>{goto("/notes/edit/"+sno.toString())}}><Pencil/></button>
+    </div>
+</form>
--- src/routes/[[app]]/notes/+page.svelte
+++ src/routes/[[app]]/notes/+page.svelte
@@ -1,15 +1,27 @@
-<script lang="ts">
+<script>
 	import NotesMini from '$lib/components/dashboard/notes_mini.svelte';
+	import Note from '$lib/components/note/note.svelte';
 	import datemap from '$lib/date';
+	import { Plus } from 'lucide-svelte';
 	import { get_type_data, shared } from '../shared.svelte';
+	import { goto } from '$app/navigation';
 	let notes = $derived(get_type_data(shared.all_data, 'notes'));
 </script>
 
-<div class="text-2xl p-5 flex flex-col">
-	Notes Page
-	<a href="/notes/new" class="text-lg underline text-blue-700 grid grid-cols-3">Create a New Note</a>
-	{#each notes as note}
-		<NotesMini name={note.Title} details={note.Content} date={datemap(note.SetDate).fulldate} />
-	{/each}
+<div class="text-2xl p-5 flex flex-col w-full">
+	<div class="flex items-center space-x-4 p-2 pb-8 pl-0">
+		<div>Notes Page</div>
+		<button class="hover:bg-gray-100 rounded-sm w-10 h-10 ring-neutral-300 ring-2 flex justify-center items-center" onclick={()=>{goto("/notes/new")}}><Plus/></button>
+	</div>
 	
+	<div class="grid grid-cols-5 gap-4">
+		{#each notes as note}
+				<Note 
+					name={note.Title}
+				 	details={note.Content} 
+					date={datemap(note.SetDate).fulldate}
+					sno={note.sno}
+				/>
+		{/each}
+	</div>
 </div>
